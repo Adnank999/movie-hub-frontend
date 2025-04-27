97d7c3cd2138e1009669c0c53d3337ff2f501ec9
Adnank999 <adnanhossain4ever@gmail.com>
2025-04-28 00:25:21 +0600

    change frontend url environtment changed port again

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,9 +1,8 @@
 
 import { io } from "socket.io-client";
 
-const socket = io(`${process.env.NEXT_PUBLIC_MOVIE_HOST}:5000`, {
+const socket = io(process.env.NEXT_PUBLIC_MOVIE_HOST, {
   withCredentials: true,
 });
-
 
 export default socket;
